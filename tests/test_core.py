335ea5d60a7e3160b3b8bde86d93689e618df215
Tobias Cadée <tobias.cadee@ticketswap.com>
2025-07-08 11:25:37 +0200

    move to uv, update deps, fix tests (#49)

--- tests/test_core.py
+++ tests/test_core.py
@@ -3,20 +3,21 @@
 import datetime
 import os
 
-from singer_sdk.testing import get_tap_test_class
+from singer_sdk.testing import SuiteConfig, get_tap_test_class
 
 from tap_intercom.tap import TapIntercom
 
 SAMPLE_CONFIG = {
-    "start_date": (datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(hours=1)).strftime(
-        "%Y-%m-%dT%H:%M:%SZ"
-    ),
+    "start_date": int((datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(hours=1)).timestamp()),
     "access_token": os.getenv("TAP_INTERCOM_ACCESS_TOKEN"),
 }
+
+TEST_SUITE_CONFIG = SuiteConfig(max_records_limit=10, ignore_no_records=True)
 
 
 # Run standard built-in tap tests from the SDK:
 TestTapIntercom = get_tap_test_class(
     tap_class=TapIntercom,
     config=SAMPLE_CONFIG,
+    suite_config=TEST_SUITE_CONFIG,
 )
