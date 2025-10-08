d5a9fd394ea917e0839e6447e490d386b0499af5
Tobias Cadee <tobias.cadee@ticketswap.com>
2025-10-08 18:45:47 +0200

    set `is_sorted` to true

--- tap_intercom/streams.py
+++ tap_intercom/streams.py
@@ -31,6 +31,7 @@ class ConversationsStream(IntercomStream):
     records_jsonpath = "$.conversations[*]"
     http_method = "POST"
     schema = conversations_schema
+    is_sorted = True
 
     def get_child_context(self, record: dict, context: dict | None) -> dict:  # noqa: ARG002
         """Return a context dictionary for child streams."""
